cc466a60e2f4eb32b9d4896d40fee691de007e53
thisara <thisara@leadinnovationz.com>
2024-10-12 23:40:15 +0530

    db

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
 const port = 4000;
+
+const mongoURI = 'mongodb+srv://thisarajack1:thisara@cluster0.yp9ld.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+// Connect to MongoDB
+mongoose.connect(mongoURI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('Error connecting to MongoDB:', err));
 
 app.get('/', (req, res) => {
   res.send('Hello, ');
